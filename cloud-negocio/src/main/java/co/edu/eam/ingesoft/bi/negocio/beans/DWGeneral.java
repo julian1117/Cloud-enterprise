5b2ec4c9af7442540b5c9bde1104ee013900336b
julian1117 <julian5296@gmail.com>
2018-05-20 12:28:58 -0500

    fecha lista en audotiria

--- cloud-negocio/src/main/java/co/edu/eam/ingesoft/bi/negocio/beans/DWGeneral.java
+++ cloud-negocio/src/main/java/co/edu/eam/ingesoft/bi/negocio/beans/DWGeneral.java
@@ -26,7 +26,7 @@ public class DWGeneral {
 	@EJB
 	private Conexion em;
 
-	public static List<Auditoria> listaTransformacion;
+	public static List<DWauditoria> listaTransformacion;
 
 	/**
 	 * Lista d elos objetos a tratar
@@ -38,34 +38,32 @@ public class DWGeneral {
 		return (List<Auditoria>) (Object) em.listar(Auditoria.AUDITORIA);
 	}
 
-	public List<Auditoria> transformacionAuditoria(int bd) throws ParseException {
+	public List<DWauditoria> transformacionAuditoria(int bd) throws ParseException {
 
-		listaTransformacion = new ArrayList<Auditoria>();
+		listaTransformacion = new ArrayList<DWauditoria>();
 		
 		em.setBd(bd);
 		List<Auditoria> list = (List<Auditoria>) (Object) em.listar(Auditoria.AUDITORIA);
 
 		String[] nav = new String[5];
+		String[] dat = new String[2];
+
 
 		for (int i = 0; i < list.size(); i++) {
 
-		
-			SimpleDateFormat formatter = new SimpleDateFormat("dd-mm-yyyy");
 			
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+			//paso date a String
+			String fecha = formatter.format(list.get(i).getFecha());			 
 			
-			int dia = list.get(i).getFecha().getDate(); 
-			int mes = list.get(i).getFecha().getMonth(); 
-			int an = list.get(i).getFecha().getYear()+1900;
-			
-			String fecha = dia + "-" + mes + "-" + an;
-			 
-			Date fech = formatter.parse(fecha);
-			 
 			nav = list.get(i).getNavegador().split(".0");
-
-			Auditoria au = new Auditoria();
+			dat = fecha.split(" ");
+			
+			System.out.println(dat[0]+"-------------------------------------------------------");	    
+			
+			DWauditoria au = new DWauditoria();
 			au.setNavegador(nav[0]);
-			au.setFecha(fech);
+			au.setFecha(dat[0]);
 			au.setOrigen(list.get(i).getOrigen());
 			au.setAccion(list.get(i).getAccion());
 			au.setNombre(list.get(i).getNombre());
