11abdf8a01a1b455ea77eedc2b39eba79a92fedd
Laura Posada <Laura Posada@DESKTOP-V90V133>
2018-05-20 18:17:14 -0500

    cambips

--- cloud-web/src/main/java/co/edu/eam/ingesoft/bi/cloud/web/controladores/GestionAdmController.java
+++ cloud-web/src/main/java/co/edu/eam/ingesoft/bi/cloud/web/controladores/GestionAdmController.java
@@ -619,6 +619,17 @@ public class GestionAdmController implements Serializable {
 		}
 
 	}
+	
+	public void enviarDatosVenta() {
+		try {
+			dwVenta.enviarTransformacionDatosVenta();
+			registrarAuditoria("Envio ventas DW", "Crear auditoria DW", "N/A");
+			Messages.addFlashGlobalInfo("Envio con éxito");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 
 	public void enviarDW() {
 		try {
